84b55338d2da85b21d2f48ddeca99258a334a6f8
Conall Gillett <conagill060@ed.amdsb.ca>
2018-10-10 14:54:30 -0400

    Add project files.

---
+++ Cash Register Summative/Form1.Designer.cs
@@ -0,0 +1,299 @@
+﻿namespace Cash_Register_Summative
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.shopnameLabel = new System.Windows.Forms.Label();
+            this.smokeyLabel = new System.Windows.Forms.Label();
+            this.tractorLabel = new System.Windows.Forms.Label();
+            this.willowLabel = new System.Windows.Forms.Label();
+            this.smokeyTextbox = new System.Windows.Forms.TextBox();
+            this.tractorTextbox = new System.Windows.Forms.TextBox();
+            this.willowTextbox = new System.Windows.Forms.TextBox();
+            this.drinkButton = new System.Windows.Forms.Button();
+            this.subtotalLabel = new System.Windows.Forms.Label();
+            this.subtotalnumberLabel = new System.Windows.Forms.Label();
+            this.taxLabel = new System.Windows.Forms.Label();
+            this.taxnumberLabel = new System.Windows.Forms.Label();
+            this.totalLabel = new System.Windows.Forms.Label();
+            this.totalnumberLabel = new System.Windows.Forms.Label();
+            this.payTextbox = new System.Windows.Forms.TextBox();
+            this.payLabel = new System.Windows.Forms.Label();
+            this.changeButton = new System.Windows.Forms.Button();
+            this.changeLabel = new System.Windows.Forms.Label();
+            this.changenumberLabel = new System.Windows.Forms.Label();
+            this.tabButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // shopnameLabel
+            // 
+            this.shopnameLabel.BackColor = System.Drawing.Color.SaddleBrown;
+            this.shopnameLabel.Font = new System.Drawing.Font("Mistral", 32.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.shopnameLabel.ForeColor = System.Drawing.Color.DarkGoldenrod;
+            this.shopnameLabel.Location = new System.Drawing.Point(-6, -7);
+            this.shopnameLabel.Name = "shopnameLabel";
+            this.shopnameLabel.Size = new System.Drawing.Size(818, 71);
+            this.shopnameLabel.TabIndex = 0;
+            this.shopnameLabel.Text = "Nith River Brewery";
+            this.shopnameLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // smokeyLabel
+            // 
+            this.smokeyLabel.Font = new System.Drawing.Font("Mistral", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.smokeyLabel.Location = new System.Drawing.Point(12, 64);
+            this.smokeyLabel.Name = "smokeyLabel";
+            this.smokeyLabel.Size = new System.Drawing.Size(141, 23);
+            this.smokeyLabel.TabIndex = 1;
+            this.smokeyLabel.Text = "Smokey Bandit\'s:";
+            // 
+            // tractorLabel
+            // 
+            this.tractorLabel.Font = new System.Drawing.Font("Mistral", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tractorLabel.Location = new System.Drawing.Point(20, 104);
+            this.tractorLabel.Name = "tractorLabel";
+            this.tractorLabel.Size = new System.Drawing.Size(100, 23);
+            this.tractorLabel.TabIndex = 2;
+            this.tractorLabel.Text = "Tractors Pull:";
+            // 
+            // willowLabel
+            // 
+            this.willowLabel.AutoSize = true;
+            this.willowLabel.Font = new System.Drawing.Font("Mistral", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.willowLabel.Location = new System.Drawing.Point(20, 146);
+            this.willowLabel.Name = "willowLabel";
+            this.willowLabel.Size = new System.Drawing.Size(72, 19);
+            this.willowLabel.TabIndex = 3;
+            this.willowLabel.Text = "Old Willow:";
+            // 
+            // smokeyTextbox
+            // 
+            this.smokeyTextbox.Font = new System.Drawing.Font("Mistral", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.smokeyTextbox.Location = new System.Drawing.Point(131, 67);
+            this.smokeyTextbox.Name = "smokeyTextbox";
+            this.smokeyTextbox.Size = new System.Drawing.Size(100, 27);
+            this.smokeyTextbox.TabIndex = 4;
+            this.smokeyTextbox.Text = "0";
+            // 
+            // tractorTextbox
+            // 
+            this.tractorTextbox.Font = new System.Drawing.Font("Mistral", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tractorTextbox.Location = new System.Drawing.Point(131, 107);
+            this.tractorTextbox.Name = "tractorTextbox";
+            this.tractorTextbox.Size = new System.Drawing.Size(100, 27);
+            this.tractorTextbox.TabIndex = 5;
+            this.tractorTextbox.Text = "0";
+            // 
+            // willowTextbox
+            // 
+            this.willowTextbox.Font = new System.Drawing.Font("Mistral", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.willowTextbox.Location = new System.Drawing.Point(131, 145);
+            this.willowTextbox.Name = "willowTextbox";
+            this.willowTextbox.Size = new System.Drawing.Size(100, 27);
+            this.willowTextbox.TabIndex = 6;
+            this.willowTextbox.Text = "0";
+            // 
+            // drinkButton
+            // 
+            this.drinkButton.Font = new System.Drawing.Font("Mistral", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.drinkButton.Location = new System.Drawing.Point(47, 180);
+            this.drinkButton.Name = "drinkButton";
+            this.drinkButton.Size = new System.Drawing.Size(158, 32);
+            this.drinkButton.TabIndex = 7;
+            this.drinkButton.Text = "Calculate Totals";
+            this.drinkButton.UseVisualStyleBackColor = true;
+            this.drinkButton.Click += new System.EventHandler(this.drinkButton_Click);
+            // 
+            // subtotalLabel
+            // 
+            this.subtotalLabel.AutoSize = true;
+            this.subtotalLabel.Font = new System.Drawing.Font("Mistral", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.subtotalLabel.Location = new System.Drawing.Point(21, 228);
+            this.subtotalLabel.Name = "subtotalLabel";
+            this.subtotalLabel.Size = new System.Drawing.Size(55, 19);
+            this.subtotalLabel.TabIndex = 8;
+            this.subtotalLabel.Text = "Subtotal:";
+            // 
+            // subtotalnumberLabel
+            // 
+            this.subtotalnumberLabel.Font = new System.Drawing.Font("Mistral", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.subtotalnumberLabel.Location = new System.Drawing.Point(84, 228);
+            this.subtotalnumberLabel.Name = "subtotalnumberLabel";
+            this.subtotalnumberLabel.Size = new System.Drawing.Size(77, 20);
+            this.subtotalnumberLabel.TabIndex = 9;
+            // 
+            // taxLabel
+            // 
+            this.taxLabel.AutoSize = true;
+            this.taxLabel.Font = new System.Drawing.Font("Mistral", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.taxLabel.Location = new System.Drawing.Point(22, 259);
+            this.taxLabel.Name = "taxLabel";
+            this.taxLabel.Size = new System.Drawing.Size(29, 19);
+            this.taxLabel.TabIndex = 10;
+            this.taxLabel.Text = "Tax:";
+            // 
+            // taxnumberLabel
+            // 
+            this.taxnumberLabel.Font = new System.Drawing.Font("Mistral", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.taxnumberLabel.Location = new System.Drawing.Point(85, 259);
+            this.taxnumberLabel.Name = "taxnumberLabel";
+            this.taxnumberLabel.Size = new System.Drawing.Size(76, 19);
+            this.taxnumberLabel.TabIndex = 11;
+            // 
+            // totalLabel
+            // 
+            this.totalLabel.Font = new System.Drawing.Font("Mistral", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.totalLabel.Location = new System.Drawing.Point(20, 291);
+            this.totalLabel.Name = "totalLabel";
+            this.totalLabel.Size = new System.Drawing.Size(44, 23);
+            this.totalLabel.TabIndex = 12;
+            this.totalLabel.Text = "Total:";
+            // 
+            // totalnumberLabel
+            // 
+            this.totalnumberLabel.Font = new System.Drawing.Font("Mistral", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.totalnumberLabel.Location = new System.Drawing.Point(85, 294);
+            this.totalnumberLabel.Name = "totalnumberLabel";
+            this.totalnumberLabel.Size = new System.Drawing.Size(100, 23);
+            this.totalnumberLabel.TabIndex = 13;
+            // 
+            // payTextbox
+            // 
+            this.payTextbox.Font = new System.Drawing.Font("Mistral", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.payTextbox.Location = new System.Drawing.Point(85, 334);
+            this.payTextbox.Name = "payTextbox";
+            this.payTextbox.Size = new System.Drawing.Size(100, 27);
+            this.payTextbox.TabIndex = 14;
+            this.payTextbox.Text = "0";
+            // 
+            // payLabel
+            // 
+            this.payLabel.Font = new System.Drawing.Font("Mistral", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.payLabel.Location = new System.Drawing.Point(12, 334);
+            this.payLabel.Name = "payLabel";
+            this.payLabel.Size = new System.Drawing.Size(67, 23);
+            this.payLabel.TabIndex = 15;
+            this.payLabel.Text = "Payment:";
+            // 
+            // changeButton
+            // 
+            this.changeButton.Font = new System.Drawing.Font("Mistral", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.changeButton.Location = new System.Drawing.Point(26, 360);
+            this.changeButton.Name = "changeButton";
+            this.changeButton.Size = new System.Drawing.Size(128, 23);
+            this.changeButton.TabIndex = 16;
+            this.changeButton.Text = "Calculate Change";
+            this.changeButton.UseVisualStyleBackColor = true;
+            this.changeButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // changeLabel
+            // 
+            this.changeLabel.Font = new System.Drawing.Font("Mistral", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.changeLabel.Location = new System.Drawing.Point(13, 398);
+            this.changeLabel.Name = "changeLabel";
+            this.changeLabel.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.changeLabel.Size = new System.Drawing.Size(119, 19);
+            this.changeLabel.TabIndex = 17;
+            this.changeLabel.Text = "Change:";
+            // 
+            // changenumberLabel
+            // 
+            this.changenumberLabel.Font = new System.Drawing.Font("Mistral", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.changenumberLabel.Location = new System.Drawing.Point(127, 394);
+            this.changenumberLabel.Name = "changenumberLabel";
+            this.changenumberLabel.Size = new System.Drawing.Size(88, 23);
+            this.changenumberLabel.TabIndex = 18;
+            // 
+            // tabButton
+            // 
+            this.tabButton.Font = new System.Drawing.Font("Mistral", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tabButton.Location = new System.Drawing.Point(26, 420);
+            this.tabButton.Name = "tabButton";
+            this.tabButton.Size = new System.Drawing.Size(94, 23);
+            this.tabButton.TabIndex = 19;
+            this.tabButton.Text = "Tab";
+            this.tabButton.UseVisualStyleBackColor = true;
+            this.tabButton.Click += new System.EventHandler(this.tabButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SaddleBrown;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabButton);
+            this.Controls.Add(this.changenumberLabel);
+            this.Controls.Add(this.changeLabel);
+            this.Controls.Add(this.changeButton);
+            this.Controls.Add(this.payLabel);
+            this.Controls.Add(this.payTextbox);
+            this.Controls.Add(this.totalnumberLabel);
+            this.Controls.Add(this.totalLabel);
+            this.Controls.Add(this.taxnumberLabel);
+            this.Controls.Add(this.taxLabel);
+            this.Controls.Add(this.subtotalnumberLabel);
+            this.Controls.Add(this.subtotalLabel);
+            this.Controls.Add(this.drinkButton);
+            this.Controls.Add(this.willowTextbox);
+            this.Controls.Add(this.tractorTextbox);
+            this.Controls.Add(this.smokeyTextbox);
+            this.Controls.Add(this.willowLabel);
+            this.Controls.Add(this.tractorLabel);
+            this.Controls.Add(this.smokeyLabel);
+            this.Controls.Add(this.shopnameLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label shopnameLabel;
+        private System.Windows.Forms.Label smokeyLabel;
+        private System.Windows.Forms.Label tractorLabel;
+        private System.Windows.Forms.Label willowLabel;
+        private System.Windows.Forms.TextBox smokeyTextbox;
+        private System.Windows.Forms.TextBox tractorTextbox;
+        private System.Windows.Forms.TextBox willowTextbox;
+        private System.Windows.Forms.Button drinkButton;
+        private System.Windows.Forms.Label subtotalLabel;
+        private System.Windows.Forms.Label subtotalnumberLabel;
+        private System.Windows.Forms.Label taxLabel;
+        private System.Windows.Forms.Label taxnumberLabel;
+        private System.Windows.Forms.Label totalLabel;
+        private System.Windows.Forms.Label totalnumberLabel;
+        private System.Windows.Forms.TextBox payTextbox;
+        private System.Windows.Forms.Label payLabel;
+        private System.Windows.Forms.Button changeButton;
+        private System.Windows.Forms.Label changeLabel;
+        private System.Windows.Forms.Label changenumberLabel;
+        private System.Windows.Forms.Button tabButton;
+    }
+}
+
---
+++ Cash Register Summative/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cash_Register_Summative
+{
+    public partial class Form1 : Form
+    {
+        const double SMOKEY = 6.99;
+        const double TRACTOR = 5.50;
+        const double WILLOW = 3;
+        const double TAX = 0.13;
+        double total = 0;
+        double tax = 0;
+        double subTotal = 0;
+        int smokeyTotal = 0;
+        int tractorTotal = 0;
+        int willowTotal = 0;
+        double willowCost;
+        double smokeyCost;
+        double tractorCost;
+        double pay = 0;
+        double change = 0;
+        bool stopCode1 = true;
+        bool stopCode2;
+        int x = 450;
+        int y = 70;
+        int height = 340;
+        int width = 250;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void drinkButton_Click(object sender, EventArgs e)
+        {
+            stopCode1 = false;
+            //input
+            try
+            {
+                smokeyTotal = Convert.ToInt16(smokeyTextbox.Text);
+                if (smokeyTotal < 0)
+                {
+                    smokeyTotal = 0;
+
+                }
+            }
+            catch
+            {
+                smokeyTotal = 0;
+            }
+            try {
+                tractorTotal = Convert.ToInt16(tractorTextbox.Text);
+                if (tractorTotal < 0)
+                {
+                    tractorTotal = 0;
+                }
+            }
+            catch
+            {
+                tractorTotal = 0;
+            }
+            try
+            {
+                willowTotal = Convert.ToInt16(willowTextbox.Text);
+                if (willowTotal < 0)
+                {
+                    willowTotal = 0;
+                }
+            }
+            catch
+            {
+                willowTotal = 0;
+
+            }
+
+
+
+            //math
+            willowCost = willowTotal * WILLOW;
+            smokeyCost = smokeyTotal * SMOKEY;
+            tractorCost = tractorTotal * TRACTOR;
+            subTotal = willowCost + smokeyCost + tractorCost;
+            tax = subTotal * TAX;
+            total = tax + subTotal;
+
+            //output
+            subtotalnumberLabel.Text = subTotal.ToString("C");
+            taxnumberLabel.Text = tax.ToString("C");
+            totalnumberLabel.Text = total.ToString("C");
+            
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (stopCode1 == false)
+            {
+                try
+                {
+                    pay = Convert.ToInt16(payTextbox.Text);
+                    if (pay < total)
+                    {
+                        changeLabel.Text = "Money Still Owed:";
+                        stopCode2 = true;
+                    }
+                    else
+                    {
+                        changeLabel.Text = "Change:";
+                        stopCode2 = false;
+                    }
+                    change = pay - total;
+
+                    changenumberLabel.Text = change.ToString("C");
+
+
+                }
+                catch
+                {
+                    changeLabel.Text = "Cash only";
+                }
+            }
+        }
+
+        private void tabButton_Click(object sender, EventArgs e)
+        {
+            if (stopCode2 == false)
+            {
+                //defining drawing 
+                Graphics g = this.CreateGraphics();
+                Font drawFont = new Font("Mistral", 12, FontStyle.Bold);
+                SolidBrush drawBrush = new SolidBrush(Color.LightGray);
+                Pen drawPen = new Pen(Color.Black, 12);
+
+                g.FillRectangle(drawBrush, x, y, width, height);
+                drawBrush.Color = Color.White;
+                Thread.Sleep(100);
+
+                while (x < 700)
+                {
+                    g.FillRectangle(drawBrush, x, y, width, height);
+
+                    Thread.Sleep(100);
+                    this.Refresh();
+
+                }
+            }
+        }
+    }
+}
